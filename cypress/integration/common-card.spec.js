43d294f2de39916436962e9862ce5626924bb9d5
Jack Zhao <yzhao@grailbio.com>
2019-04-23 02:40:50 +0000

    lims/client: fix cypress tests
    Summary: Fix broken tests.
    
    Test Plan: cypress
    
    Reviewers: nsawas, margi, jsingh, O13 UI Common, O12 LIMS UI, ecarrel
    
    Reviewed By: jsingh, O13 UI Common, O12 LIMS UI, ecarrel
    
    Maniphest Tasks: T8000
    
    Differential Revision: https://phabricator.grailbio.com/D28992
    
    fbshipit-source-id: 17a99e4

--- cypress/integration/common-card.spec.js
+++ cypress/integration/common-card.spec.js
@@ -2,6 +2,6 @@ describe("Common Card", () => {
   it("successfully loads", () => {
     cy.visit("/#!/CommonCard");
     cy.get("[data-testid=card-header]").should("contain", "New Instrument");
-    cy.get("[data-testid=card-content]").should("contain", "Your Card");
+    cy.get("[data-testid=card-body]").should("contain", "Your Card");
   });
 });

394a4dc240ec4aceff6c29955e245362eed3553f
Jack Zhao <yzhao@grailbio.com>
2019-06-07 14:11:49 -0700

    lims: add tags to samples
    Summary:
    For stored samples, assay samples, and study samples, allow the user to search for applied tags.
    Show tags in tables view and details view.
    
    Test Plan: Added tags in db, ensure they show up in UI in various views.
    
    Reviewers: ecarrel, nsawas, rhuang, O10 LIMS Server, O12 LIMS UI, O13 UI Common, jsingh
    
    Reviewed By: rhuang, O10 LIMS Server, O12 LIMS UI, O13 UI Common
    
    Subscribers: jsingh, cji
    
    Maniphest Tasks: T15622
    
    Differential Revision: https://phabricator.grailbio.com/D31597
    
    fbshipit-source-id: 77b0b65

--- src/link/external-link.jsx
+++ src/link/external-link.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { type Node } from "react";
+import React, { type Node, forwardRef } from "react";
 
 type Props = {
   /** Takes a node to include in the omni dropdown after the search fields */
@@ -14,7 +14,7 @@ type Props = {
  *  ExternalLink opens in a new tab, while ensuring that the link protects from:
  * https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
  */
-export const ExternalLink = React.forwardRef<Props, any>((props: Props, ref: any) => {
+export const ExternalLink = forwardRef<Props, any>((props: Props, ref: any) => {
   const { children, "data-testid": dataTestId = "external-link", ...aTagProps } = props;
   return (
     <a

bde64e6de5d8c06756d8cb75316980e3944f1441
Jack Zhao <yzhao@grailbio.com>
2019-06-25 17:14:20 -0700

    ui/common: build for ie11
    Summary: Target ie 11 in browserslist so babel can compile to target version.
    
    Test Plan:
    yarn test && yarn build
    launched client
    
    Reviewers: O13 UI Common, nsawas, jsingh
    
    Reviewed By: O13 UI Common, jsingh
    
    Subscribers: ecarrel, mfilippova
    
    Maniphest Tasks: T8000
    
    Differential Revision: https://phabricator.grailbio.com/D32564
    
    fbshipit-source-id: 10646cf

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-// @flow
 module.exports = {
-  presets: ["kcd-scripts/babel"],
+  presets: [require("@babel/preset-env"), require("@babel/preset-flow"), require("@babel/preset-react")],
+  plugins: [require("@babel/plugin-proposal-class-properties"), require("@babel/plugin-transform-runtime")],
 };
